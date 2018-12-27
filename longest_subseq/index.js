8b1de47b2a4d6d58dc00eff9a46ba182dd9f5f3a
Marcus Bizal <hello@marcbizal.com>
2018-12-27 11:07:54 -0500

    memoized unionStrings in longest_subseq kata

--- longest_subseq/index.js
+++ longest_subseq/index.js
@@ -1,13 +1,21 @@
-// const memoize = require("lodash.memoize");
+const memo = fn => {
+  const cache = {};
+  return (s1, s2, start1, start2) => {
+    const key = `${s1} ∪ ${s2}`;
+    const index = Math.pow(2, start1) * Math.pow(3, start2);
+    if (key in cache && index in cache[key]) {
+      return cache[key][index];
+    } else {
+      const result = fn(s1, s2, start1, start2);
+      cache[key] = { ...cache[key], [index]: result };
+      return result;
+    }
+  };
+};
 
-/*
- *  TODO: Write resolver for memoize since it currently only supports a single argument
- *  unionStrings might need a slight implementation change to support this.
- */
-
-const unionStrings = (s1, s2, start1, start2) => {
+const unionStrings = memo((s1, s2, start1, start2) => {
   if (!s1.length || !s2.length) return "";
-  if (start1 > s1.length || start2 > s2.length) return "";
+  if (start1 >= s1.length || start2 >= s2.length) return "";
 
   const ch = s1[start1];
   const iof = s2.indexOf(ch, start2);
@@ -15,7 +23,7 @@ const unionStrings = (s1, s2, start1, start2) => {
   return iof >= 0
     ? ch + unionStrings(s1, s2, start1 + 1, iof + 1)
     : unionStrings(s1, s2, start1 + 1, start2);
-};
+});
 
 const longestString = (acc, cur) => (cur.length > acc.length ? cur : acc);
 
