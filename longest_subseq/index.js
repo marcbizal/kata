154076dad8b1f6ed1e5b1b73ff8a43e9cc70092e
Marcus Bizal <hello@marcbizal.com>
2018-12-27 10:48:03 -0500

    slight implementation change to longest_subseq to prepare to memoize

--- longest_subseq/index.js
+++ longest_subseq/index.js
@@ -5,15 +5,16 @@
  *  unionStrings might need a slight implementation change to support this.
  */
 
-const unionStrings = (s1, s2) => {
+const unionStrings = (s1, s2, start1, start2) => {
   if (!s1.length || !s2.length) return "";
+  if (start1 > s1.length || start2 > s2.length) return "";
 
-  const ch = s1[0];
-  const iof = s2.indexOf(ch);
+  const ch = s1[start1];
+  const iof = s2.indexOf(ch, start2);
 
   return iof >= 0
-    ? ch + unionStrings(s1.substring(1), s2.substring(iof + 1))
-    : unionStrings(s1.substring(1), s2);
+    ? ch + unionStrings(s1, s2, start1 + 1, iof + 1)
+    : unionStrings(s1, s2, start1 + 1, start2);
 };
 
 const longestString = (acc, cur) => (cur.length > acc.length ? cur : acc);
@@ -21,7 +22,7 @@ const longestString = (acc, cur) => (cur.length > acc.length ? cur : acc);
 const longestSubseq = (s1, s2) =>
   s1
     .split("")
-    .map((_, start) => unionStrings(s1.substring(start), s2))
+    .map((_, start) => unionStrings(s1, s2, start, 0))
     .reduce(longestString, "");
 
 module.exports = longestSubseq;
